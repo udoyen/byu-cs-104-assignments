d6dfe17ad1de019f2c1d8232f388bf71144d20c8
george udosen <datameshprojects@gmail.com>
2024-05-18 10:22:47 +0100

    Updates accessAPI file

--- week5/accessAPI.py
+++ week5/accessAPI.py
@@ -1,41 +1,68 @@
+import sys
+
 import requests
 import json
 
 base_url = 'https://openscriptureapi.org/api/scriptures/v1/lds/en/volume/bookofmormon/'
 
-
 print("Welcome to the Book of Mormon Summary Tool!")
 
-user_response = True # set the initial response to True
+user_response = True  # set the initial response to True
 
-while user_response: # Check user response state
+
+def check_requested_book():
+    book_check_response = requests.get(base_url)
+    b_check = book_check_response.json()
+    list_of_books = [_id['_id'] for _id in b_check['books']]
+    if book_request.replace(" ", "").lower() not in list_of_books:
+        sys.exit(1)
+
+
+def check_requested_chapter():
+    chapter_check = requests.get(base_url + book_request.replace(" ", ""))
+    c_check = chapter_check.json()
+    list_of_chapters = [_id['_id'][len(book_request.replace(" ", "").lower()):] for _id in c_check['chapters']]
+    if chapter_request not in list_of_chapters:
+        print(
+            f"That chapter doesn't exist, please enter a number between {list_of_chapters[0]} and {list_of_chapters[-1]}")
+        sys.exit(1)
+
+
+while user_response:  # Check user response state
 
     chapter_num = ""
     book_name = ""
 
-    book_request = input("Which book of the Book of Mormon would you like? ") # ask user for book name
+    book_request = input("Which book of the Book of Mormon would you like? ")  # ask user for book name
 
-    is_name_the_same = book_request == book_request.replace(" ", "") # check to see if book name is composed of two parts
+    # check if request book exists
+    check_requested_book()
 
-    if not is_name_the_same: # handles Book of Mormon names with numbers e.g. 1 Nephi
-        split_it = book_request.split(" ") # split the name using the included spaces
+    is_name_the_same = book_request == book_request.replace(" ",
+                                                            "")  # check to see if book name is composed of two parts
+
+    if not is_name_the_same:  # handles Book of Mormon names with numbers e.g. 1 Nephi
+        split_it = book_request.split(" ")  # split the name using the included spaces
         chapter_num = split_it[0]
         book_name = split_it[1]
 
+    chapter_request = input(
+        f"Which chapter of {chapter_num + ' ' + book_name.capitalize() if not is_name_the_same else book_request.capitalize()} are you interested in? ")  # ask user for chapter
 
-    chapter_request = input(f"Which chapter of {chapter_num + ' ' + book_name.capitalize() if not is_name_the_same else book_request.capitalize()} are you interested in? ") # ask user for chapter
+    # check if requested chapter exists
+    check_requested_chapter()
 
-    response = requests.get(base_url + book_request.replace(" ", "") + '/' + chapter_request)  # Use the url string to ask for the content
-    data = response.json()  #We want the data returned as json
+    response = requests.get(
+        base_url + book_request.replace(" ", "") + '/' + chapter_request)  # Use the url string to ask for the content
+    data = response.json()  # We want the data returned as json
 
-    print(f"Summary of {chapter_num + ' ' + book_name.capitalize() if not is_name_the_same else book_request.capitalize()} chapter {chapter_request}:")
+    print(
+        f"Summary of {chapter_num + ' ' + book_name.capitalize() if not is_name_the_same else book_request.capitalize()} chapter {chapter_request}:")
 
-    
-    print('--' + data['chapter']['summary'],"\n")
+    print('--' + data['chapter']['summary'], "\n")
 
     answer = input("Would you like to view another (Y/N)? ")
-    if answer.upper() == "N": # ask user if they want to continue
+    if answer.upper() == "N":  # ask user if they want to continue
         user_response = False
-
 
 print("Thank you for using Book of Mormon Summary Tool!")
--- week5/myfinance.py
+++ week5/myfinance.py
@@ -10,6 +10,7 @@ def gross_pay(wage, work_duration):
     """
     return float(work_duration) * float(wage)
 
+
 def federal_tax(gross_pay, percentage=10):
     """function to calculate federal tax for username
 
@@ -20,7 +21,8 @@ def federal_tax(gross_pay, percentage=10):
     Returns:
         float: Deducted tax amount
     """
-    return (percentage/100) * gross_pay
+    return (percentage / 100) * gross_pay
+
 
 def state_tax(gross_pay, percentage=5):
     """Function to calculate state tax for users
@@ -32,7 +34,8 @@ def state_tax(gross_pay, percentage=5):
     Returns:
         float: Deducted tax amount
     """
-    return (percentage/100) * gross_pay
+    return (percentage / 100) * gross_pay
+
 
 def social_security(gross_pay, percentage=6.2):
     """function to calculate social security
@@ -44,7 +47,8 @@ def social_security(gross_pay, percentage=6.2):
     Returns:
         float: deducted tax amount
     """
-    return (percentage/100) * gross_pay
+    return (percentage / 100) * gross_pay
+
 
 def net_pay():
     """function to calculate net pay for users
@@ -52,21 +56,23 @@ def net_pay():
     Returns:
         string: string representing gross pay, federal tax, state tax, social security, and net pay
     """
-    
+
     wage = input("What is your hourly wage? ")
     work_duration = input("How many hours did you work? ")
-    
+
     grossPay = gross_pay(wage, work_duration)
-    
+
     federalTax = federal_tax(grossPay)
     stateTax = state_tax(grossPay)
     socialSecurityTax = social_security(grossPay)
-    
+
     withholding_sum = withholding(federalTax, stateTax, socialSecurityTax)
-    
+
     net_pay = grossPay - withholding_sum
-    
-    return "Gross Pay: $%0.2f(%0.1f hours @ $%0.2f/hr)\nFederal tax: $%0.2f\nState tax: $%0.2f\nSocial security: $%0.2f\nNet pay: $%0.2f" % (grossPay, float(work_duration), float(wage), federalTax, stateTax, socialSecurityTax, net_pay)
+
+    return "Gross Pay: $%0.2f(%0.1f hours @ $%0.2f/hr)\nFederal tax: $%0.2f\nState tax: $%0.2f\nSocial security: $%0.2f\nNet pay: $%0.2f" % (
+    grossPay, float(work_duration), float(wage), federalTax, stateTax, socialSecurityTax, net_pay)
+
 
 def withholding(federalTax, stateTax, socialSecurityTax):
     """function to calculate withholding elements sum
@@ -81,6 +87,7 @@ def withholding(federalTax, stateTax, socialSecurityTax):
     """
     return sum([federalTax, stateTax, socialSecurityTax])
 
+
 def show_discretionary_income(dict_of_revenue_and_expenses):
     """function to show discretionary income for users
 
@@ -90,11 +97,12 @@ def show_discretionary_income(dict_of_revenue_and_expenses):
     Returns:
         string: string values for revenue, expenses and discretionary values
     """
-    
-    revenue = sum([v for v in dict_of_revenue_and_expenses.values() if v > 0]) 
-    expenses = sum([v for v in dict_of_revenue_and_expenses.values() if v < 0]) 
-    
+
+    revenue = sum([v for v in dict_of_revenue_and_expenses.values() if v > 0])
+    expenses = sum([v for v in dict_of_revenue_and_expenses.values() if v < 0])
+
     return "Revenue: $%0.2f Expenses: $%0.2f Discretionary: $%0.2f" % (revenue, expenses, revenue + expenses)
+
 
 def get_revenue_or_expenses():
     """function to get the revenue and expenses of users
@@ -102,16 +110,16 @@ def get_revenue_or_expenses():
     Returns:
         dictionary: dictionary of revenue and expenses
     """
-    
+
     user_response = True
     income_and_expenses_data = {}
-    
+
     while user_response:
-    
+
         transaction_name = input("Enter transaction name: ")
         amount = input("Enter amount (use negative sign for expense): ")
-        print() # print a blank line
-        
+        print()  # print a blank line
+
         if transaction_name.upper() in income_and_expenses_data:
             if amount[0] == "-":
                 income_and_expenses_data[transaction_name.upper()] -= float(amount[1:])
@@ -119,14 +127,12 @@ def get_revenue_or_expenses():
                 income_and_expenses_data[transaction_name.upper()] += float(amount)
         else:
             income_and_expenses_data[transaction_name.upper()] = float(amount)
-            
-            
+
         another = input("Another? (Y/N): ")
-            
-            
+
         if another.upper() == "N":
             user_response = False
-    
+
     return income_and_expenses_data
 
 
@@ -137,31 +143,33 @@ def show_menu():
         string: string representation of the user's choice between 1 & 4
     """
     choice = input("1-Calculate net pay\n2-Enter revenue or expense\n3-Show discretionary income\n4-Exit\nChoice ")
-    print() # print a blank line
+    print()  # print a blank line
     return choice
 
+
 def main():
-    """main entry point for the applicaton
+    """main entry point for the application
     """
     state = True
-    
+
     get_revenue_or_expenses_data = ""
-    
+
     while state:
         choice = show_menu()
         if choice == "1":
             print(net_pay())
-            print() # print a blank line        
+            print()  # print a blank line
         elif choice == "2":
             get_revenue_or_expenses_data = get_revenue_or_expenses()
         elif choice == "3":
             print(show_discretionary_income(get_revenue_or_expenses_data))
-            print() # print a blank line
+            print()  # print a blank line
         elif choice == "4":
             state = False
             print("Thanks for using My Finance!")
         else:
             print("Please enter a valid choice betwee 1 and 4!")
-        
+
+
 if __name__ == "__main__":
     main()
